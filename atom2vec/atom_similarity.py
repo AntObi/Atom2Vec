61bf1d90e2527f39a6d96372bfd76f80cfadfd72
Anthony Onwuli <anthony.onwuli16@imperial.ac.uk>
2023-05-23 16:40:05 +0100

    created SpeciesSimilarity class

--- atom2vec/atom_similarity.py
+++ atom2vec/atom_similarity.py
@@ -6,8 +6,8 @@ from collections import defaultdict
 from typing import Any, Dict, List, Set, Tuple, Union, AnyStr
 
 import numpy as np
-from pymatgen.core import Element, Structure, Composition
-from pymatgen.core.periodic_table import get_el_sp
+from pymatgen.core import  Structure, Composition
+from pymatgen.core.periodic_table import get_el_sp, Element, Species
 from scipy.sparse.linalg import svds
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
@@ -125,6 +125,124 @@ class AtomSimilarity:
             "atoms_similarity": self._atoms_similarity,
             "atoms_vector": self._atoms_vector,
             "max_elements": self._max_elements,
+        }
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(**d)
+
+
+class SpeciesSimilarity:
+    """
+    Compute the similarity of species
+    """
+
+    def __init__(self, max_species, species_vector, species_similarity):
+        self._max_species: int = max_species
+        self._k_dim = len(next(iter(species_vector.values())))
+        self._species_vector: Dict[str, List[float]] = species_vector
+        self._species_similarity: Dict[str, Dict[str, float]] = species_similarity
+
+    @property
+    def max_species(self):
+        return self._max_species
+
+    @property
+    def k_dim(self):
+        return self._k_dim
+
+    @classmethod
+    def _kwargs_from_structures(cls, structures: List[Structure], k_dim: int,
+                                max_species: int) -> Dict[str, Any]:
+        compositions = [s.composition.reduced_composition for s in structures
+                        if 1 < len(s.composition.elements) <= max_species]
+
+        env_dict = defaultdict(list)
+        species_set: Set[Species] = set()
+
+        for composition in tqdm(compositions, desc="Generating the environment matrix"):
+            composition_items = composition.items()
+            for s, amount in composition_items:
+                env_composition = composition - Composition({s: amount})
+                env_dict[env_composition].append(s)
+                species_set.add(s)
+
+        species = {s: i for i, s in enumerate(list(species_set))}
+
+        env_matrix = np.zeros((len(species), len(env_dict)), dtype=np.int8)
+        for j, species_ in enumerate(env_dict.values()):
+            for s in species_:
+                i = species[s]
+                env_matrix[i][j] = 1
+
+        u, d, v = svds(env_matrix.astype(dtype=np.float32), k=k_dim, which="LM")
+        species_vector = u @ np.diag(d)
+        species_vector_dict = {s.to_pretty_string(): species_vector[i].tolist() for s, i in species.items()}
+
+        species_similarity = cosine_similarity(species_vector)
+        species_similarity_dict = {s.to_pretty_string(): {_s.to_pretty_string(): float(species_similarity[i, _i])
+                                          for _s, _i in species.items()}
+                                 for s, i in species.items()}
+
+        return {
+            "species_similarity": species_similarity_dict,
+            "species_vector": species_vector_dict,
+            "max_species": max_species,
+        }
+
+    @classmethod
+    def from_structures(cls, structures: List[Structure],
+                        k_dim: int, max_species: int):
+        """
+        Generating species vectors and species similarity matrix from list of :obj:`pymatgen.core.Structure`
+
+        Args:
+            structures: list of structures
+            k_dim: the dimension of species vectors, recommended value: [50, 100, 300]
+            max_elements: if the number of species in a structure exceeds `max_species`, it will be
+                automatically ignored to save space as a species environment of too many species are
+                very rare, recommended value: 3
+        """
+        return cls(**cls._kwargs_from_structures(structures=structures,
+                                                 k_dim=k_dim, max_species=max_species))
+
+    def get_species_vector(self, item: Union[Species, AnyStr, int]) -> List[float]:
+        """
+        Query species vector
+
+        Raises:
+            KeyError: when the species requested do not exist.
+        """
+        e = get_el_sp(item)
+        try:
+            return self._species_vector[e.to_pretty_string()]
+        except KeyError as err:
+            err.args = ("Not such species as {}".format(item),)
+            raise
+
+    def get_species_similarity(self, item: Tuple[Union[Species, AnyStr]]) -> float:
+        """
+        Query cosine similarity of two species
+
+        The data type of species can be:
+            1) `pymatgen.core.Species` enum type
+            2) species string
+
+        If neither species are not presented in the training set, -1. will be returned.
+        """
+        if not isinstance(item, tuple) and len(item) != 2:
+            raise ValueError(f"Expect two species tuple, but get {item}")
+        e_1, e_2 = get_el_sp(item[0]), get_el_sp(item[1])
+        return self._species_similarity.get(e_1.to_pretty_string(), {}).get(e_2.to_pretty_string(), -1.)
+
+    def __getitem__(self, item: Tuple[Union[Species, AnyStr]]):
+        return self.get_species_similarity(item)
+
+    def as_dict(self):
+        return {
+            "species_similarity": self._species_similarity,
+            "species_vector": self._species_vector,
+            "max_species": self._max_species,
         }
 
     @classmethod
